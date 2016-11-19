309d6a504cfbd8a1a2e91645e69c71e9a485edce
Marcin Lulek <info@webreactor.eu>
2016-11-19 09:47:02 +0100

    i18n: tests

---
+++ testscaffold/tests/functional/test_i18n.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import pytest
+
+from six.moves.urllib import parse
+
+from testscaffold.tests.utils import (
+    create_user,
+    session_context,
+    create_admin)
+
+
+@pytest.mark.usefixtures('full_app', 'with_migrations', 'clean_tables',
+                         'sqla_session')
+class TestI18N(object):
+    def test_default_en_translation(self, full_app):
+        url_path = '/'
+        response = full_app.get(url_path, {}, status=200)
+        assert 'Some project name' in response.text
+        assert 'Register here' in response.text
+        assert 'Password' in response.text
+
+    def test_pl_translation(self, full_app):
+        url_path = '/?_LOCALE_=pl'
+        response = full_app.get(url_path, {}, status=200)
+        assert 'Nazwa projektu' in response.text
+        assert 'Rejestruj się' in response.text
+        assert 'Hasło' in response.text
